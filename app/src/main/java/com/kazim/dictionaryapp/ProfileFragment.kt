32ad7861e6cd3a49fc9da2d3baa2913ed62712ac
Kazım Muhammet Temiz <kzmtmz52@gmail.com>
2021-05-30 22:56:24 +0300

    auth

--- app/src/main/java/com/kazim/dictionaryapp/ProfileFragment.kt
+++ app/src/main/java/com/kazim/dictionaryapp/ProfileFragment.kt
@@ -12,7 +12,6 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.kazim.dictionaryapp.databinding.ActivityAddWordBinding
 import com.kazim.dictionaryapp.databinding.FragmentProfileBinding
 import java.lang.StringBuilder
 import java.util.zip.Inflater
